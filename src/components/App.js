a36951d053c6cff19b21001a887c9b9e8bc384b7
Dominique Loulie <dom.loulie@gmail.com>
2023-06-08 14:51:06 +1000

    this is my first

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 
+
 function App() {
-  const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
-  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+  const title ="Akira";
+  
+  const  {postersurl}  ="https://flxt.tmsimg.com/assets/p16852_p_v8_ad.jpg";
+  const genresArr = [" Sci-fi, Cyberpunk, Action, Fantasy, Thriller, Adventure, Drama, Apocalyptic and post-aspocalyptic fiction, Anime"];
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      {/*passing down the props from the parent component */}
+      <MovieCard
+       title={title} genres={genresArr} />
     </div>
   );
 }
